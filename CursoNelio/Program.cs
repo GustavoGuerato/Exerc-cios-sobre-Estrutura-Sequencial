7933ddd464eaa2f77c6f3c8060dd71fec0cebb7b
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-02-08 13:29:49 -0300

    alguns exercicios para fixação em um unico arquivo cs

---
+++ CursoNelio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Formats.Asn1;
+
+namespace CursoNelio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"a soma é de {a + b}");
+
+            //double raio = double.Parse(Console.ReadLine());
+            //double pi = 3.14159;
+            //double area = pi * Math.Pow(raio, 2);
+            //Console.WriteLine(area);
+
+            //int A = int.Parse(Console.ReadLine());
+            //int B = int.Parse(Console.ReadLine());
+            //int C = int.Parse(Console.ReadLine());
+            //int D = int.Parse(Console.ReadLine());
+            //int diferenca = (A * B - C * D);
+            //Console.WriteLine(diferenca);
+
+            int serial = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valorHora = double.Parse(Console.ReadLine());
+            double salario = horas * valorHora;
+            Console.WriteLine($"number = {serial}");
+            Console.WriteLine($"salary = ${salario}");
+
+        }
+    }
+}
